b10e4b4ab3038821128a3c64fc3f4d350dee1a26
Siddharth9616 <siddharthrai251@gmail.com>
2025-08-29 09:33:53 +0530

    Adding pairsum in arrayList folder

--- Container_with_most_water.java
+++ Container_with_most_water.java
@@ -2,17 +2,41 @@
 import java.util.ArrayList;
 
 public class Container_with_most_water {
+    // public static int storeWater(ArrayList<Integer> height){
+    //     int maxWater = 0;
+    //     for(int i=0; i<height.size(); i++){
+    //         for(int j=i+1; j<height.size(); j++){
+    //             int ht = Math.min(height.get(i), height.get(j));
+    //             int width = j-i;
+    //             int currWater = ht * width;
+    //             maxWater = Math.max(maxWater, currWater);
+    //         }
+    //     }
+    //     return maxWater;
+    // }
+
+
+    // 2 pointer approch
     public static int storeWater(ArrayList<Integer> height){
-        int maxWater = 0;
-        for(int i=0; i<height.size(); i++){
-            for(int j=i+1; j<height.size(); j++){
-                int ht = Math.min(height.get(i), height.get(j));
-                int width = j-i;
-                int currWater = ht * width;
-                maxWater = Math.max(maxWater, currWater);
+        int maxWater=0;
+        int lp = 0;
+        int rp = height.size()-1;
+        while(lp<rp){
+            //calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp-lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            //update ptr
+            if(height.get(lp)<height.get(rp)){
+                lp++;
+            }else {
+                rp--;
             }
         }
-        return maxWater;
+        return  maxWater;
+
     }
 
     public static void main(String[] args) {
